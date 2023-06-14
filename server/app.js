9ff3a43b1074a977d016236220f72d63db51518f
Gracious Nguwo <graciousnguwo2020@gmail.com>
2023-06-14 11:07:18 +0200

    completed API development

--- server/app.js
+++ server/app.js
@@ -17,25 +17,6 @@ const CONNECTION = process.env.CONNECTION;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const customers = [
-//   {
-//     name: "Gracious",
-//     industry: "IT",
-//   },
-//   {
-//     name: "Rejoice",
-//     industry: "Health",
-//   },
-//   {
-//     name: "Joshua",
-//     industry: "IT",
-//   },
-//   {
-//     name: "Priscilla",
-//     industry: "Social Work",
-//   },
-// ];
-
 const customer = new Customer({
   name: "Gracious",
   industry: "IT",
@@ -45,6 +26,7 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
+//endpoint to list all customers
 app.get("/api/customers", async (req, res) => {
   console.log(await mongoose.connection.db.listCollections().toArray());
   try {
@@ -55,23 +37,60 @@ app.get("/api/customers", async (req, res) => {
   }
 });
 
-app.get('/api/customers/:id', async(req, res) => {
-  res.json({
+// getting data by ID
+app.get("/api/customers/:id", async (req, res) => {
+  console.log({
     requestParams: req.params,
-    requestQuery: req.query
-  })
+    requestQuery: req.query,
+  });
+  try {
+    const { id: customerId } = req.params;
+    console.log(customerId);
+    const customer = await Customer.findById(customerId);
+    console.log(customer);
+    if (!customer) {
+      res.status(404).json({ error: "Customer not found" });
+    } else {
+      res.json({ customer });
+    }
+  } catch (err) {
+    res.status(500).json({ error: "something went wrong" });
+  }
 });
 
-app.post("/api/customers", async(req, res) => {
+// endpoint for updating customer
+app.put("/api/customers/:id", async (req, res) => {
+  try {
+    const customerId = req.params.id;
+    const result = await Customer.replaceOne({ _id: customerId }, req.body);
+    console.log(result);
+    res.json({ updatedCount: result.modifiedCount });
+  } catch (err) {
+    res.status(500).json({ err: "something went wrong" });
+  }
+});
+
+//endpoint for deleting a customer by ID
+app.delete("/api/customers/:id", async (req, res) => {
+  try {
+    const customerId = req.params.id;
+    const result = await Customer.deleteOne({ _id: customerId });
+    res.json({ deletedCount: result.deletedCount });
+  } catch (err) {
+    res.status(500).json({ err: "something went wrong" });
+  }
+});
+
+//endpoint for adding new customer
+app.post("/api/customers", async (req, res) => {
   console.log(req.body);
   const customer = new Customer(req.body);
   try {
     await customer.save();
-    res.status(201).json({customer});
-  } catch(err) {
-    res.status(400).json({error: err.message});
+    res.status(201).json({ customer });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
   }
-  
 });
 
 const start = async () => {
